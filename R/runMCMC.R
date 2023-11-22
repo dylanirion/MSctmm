768db11b657295403777324a99a312ddabef7fb6
dylanirion <dylan.irion@comcast.net>
2023-11-22 12:14:16 +0200

    last fix to proposeParams indexing

--- R/runMCMC.R
+++ R/runMCMC.R
@@ -597,6 +597,7 @@ proposeParams <- function(param, fixedParams, S, nbStates) {
   if (length(param) == 3 * nbStates) {
     thetasprime[is.na(unlist(fixedParams))] <- exp(thetas[is.na(unlist(fixedParams))])
   } else if (length(param) == 5 * nbStates) {
+    thetasprime[which(is.na(unlist(fixedParams)))] <- thetas[which(is.na(unlist(fixedParams)))]
     thetasprime[which(seq_len(length(param)) %% 5 != 0 & is.na(unlist(fixedParams)))] <- exp(thetas[which(seq_len(length(param)) %% 5 != 0 & is.na(unlist(fixedParams)))])
   }
   return(list(u, thetasprime))
