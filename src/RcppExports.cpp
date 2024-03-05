7df418c55d66cc890cd3907d27aa1bfd2a80299b
dylanirion <dylan.irion@comcast.net>
2024-03-05 14:10:07 +0200

    Rcpp update

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -81,7 +81,7 @@ RcppExport SEXP _MSctmm_sample_path_mr(SEXP aSEXP, SEXP bSEXP, SEXP t0SEXP, SEXP
     if (rcpp_isError_gen) {
         SEXP rcpp_msgSEXP_gen = Rf_asChar(rcpp_result_gen);
         UNPROTECT(1);
-        Rf_error(CHAR(rcpp_msgSEXP_gen));
+        Rf_error("%s", CHAR(rcpp_msgSEXP_gen));
     }
     UNPROTECT(1);
     return rcpp_result_gen;
@@ -131,7 +131,7 @@ RcppExport SEXP _MSctmm_sample_path_mr2(SEXP aSEXP, SEXP bSEXP, SEXP t0SEXP, SEX
     if (rcpp_isError_gen) {
         SEXP rcpp_msgSEXP_gen = Rf_asChar(rcpp_result_gen);
         UNPROTECT(1);
-        Rf_error(CHAR(rcpp_msgSEXP_gen));
+        Rf_error("%s", CHAR(rcpp_msgSEXP_gen));
     }
     UNPROTECT(1);
     return rcpp_result_gen;
