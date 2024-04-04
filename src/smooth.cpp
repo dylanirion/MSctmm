98c431ad2d8c05c0f177fa56bf6247f612b41fc1
dylanirion <dylan.irion@comcast.net>
2024-04-04 20:41:40 +0200

    cleanup

--- src/smooth.cpp
+++ src/smooth.cpp
@@ -38,176 +38,176 @@ using namespace arma;
 //'
 //' @export
 // [[Rcpp::export]]
-List smooth_rcpp(const arma::mat& data, int nbStates, const arma::vec param, const arma::vec fixmu, const arma::mat& Hmat) {
+List smooth_rcpp(const arma::mat &data, int nbStates, const arma::vec param, const arma::vec fixmu, const arma::mat &Hmat)
+{
   int nbData = data.n_rows;
-  int nbID = 0;
 
   // unpack data
-  mat X = data.cols( 0, 1 );    // x, y
-  vec time = data.col( 2 );     // time
-  vec ID = data.col( 3 );       // ID
-  vec S = data.col( 4 );        // state
-  vec dt( nbData, fill::ones ); // time intervals
+  mat X = data.cols(0, 1);    // x, y
+  vec time = data.col(2);     // time
+  vec ID = data.col(3);       // ID
+  vec S = data.col(4);        // state
+  vec dt(nbData, fill::ones); // time intervals
   dt(0) = datum::inf;
-  dt.subvec( 1, nbData - 1 ) = diff( time );
+  dt.subvec(1, nbData - 1) = diff(time);
 
-  mat aest( nbData, 4, fill::zeros );  // state estimate
-  cube Pest( 4, 4, nbData, fill::zeros );  //covariance estimate
-  mat afor( nbData, 4, fill::zeros );  // state forecast
-  cube Pfor( 4, 4, nbData, fill::zeros );  //covariance forecast
+  mat aest(nbData, 4, fill::zeros);     // state estimate
+  cube Pest(4, 4, nbData, fill::zeros); // covariance estimate
+  mat afor(nbData, 4, fill::zeros);     // state forecast
+  cube Pfor(4, 4, nbData, fill::zeros); // covariance forecast
 
   // unpack parameters
-  vec tau_pos = param.subvec( 0, nbStates - 1 );
-  vec tau_vel = param.subvec( nbStates, 2 * nbStates - 1 );
+  vec tau_pos = param.subvec(0, nbStates - 1);
+  vec tau_vel = param.subvec(nbStates, 2 * nbStates - 1);
   cube sigma(2, 2, nbStates);
-  for(int i = 0; i < nbStates; i++) {
-    if (param.size() / nbStates == 3) {
-      sigma.slice(i)(0,0) = param.subvec(2 * nbStates, 3 * nbStates - 1)(i);
-      sigma.slice(i)(1,1) = param.subvec(2 * nbStates, 3 * nbStates - 1)(i);
-    } else if (param.size() / nbStates == 5) {
+  for (int i = 0; i < nbStates; i++)
+  {
+    if (param.size() / nbStates == 3)
+    {
+      sigma.slice(i)(0, 0) = param.subvec(2 * nbStates, 3 * nbStates - 1)(i);
+      sigma.slice(i)(1, 1) = param.subvec(2 * nbStates, 3 * nbStates - 1)(i);
+    }
+    else if (param.size() / nbStates == 5)
+    {
       sigma.slice(i).diag() = param.subvec(2 * nbStates + (3 * i), 2 * nbStates + (3 * i) + 1);
-      sigma.slice(i)(0,1) = param(2 * nbStates + (3 * i + 1));
-      sigma.slice(i)(1,0) = param(2 * nbStates + (3 * i + 1));
+      sigma.slice(i)(0, 1) = param(2 * nbStates + (3 * i + 1));
+      sigma.slice(i)(1, 0) = param(2 * nbStates + (3 * i + 1));
     }
   }
 
   // define all empty matrices and vectors needed for the Kalman Filter and Smoother
-  mat Z { { 1, 0 ,0 ,0 }, { 0, 0, 1, 0 } }; // observation model which maps the true state space into the observed space ( P, Hk )
-  mat I( 4, 4, fill::eye );   // identity matrix
-  mat H( 2, 2, fill::zeros ); // the covariance of the observation noise ( error, Rk )
-  cube T( 4, 4, nbData, fill::zeros );              // state transition model which is applied to the previous state xk−1 ( Green, Fk )
-  cube Q( 4, 4, nbData, fill::zeros );              // covariance of the process noise ( Sigma, Q )
-  mat K( 4, 2, fill::zeros ); // Kalman Gain ( Gain, Kk )
-  mat u( nbData, 2, fill::zeros );      // measurement residual ( zRes, yk )
-  cube iF( 2, 2, nbData, fill::zeros ); // inverse of residual covariance, residual "precision" ( isRes )
+  mat Z{{1, 0, 0, 0}, {0, 0, 1, 0}};  // observation model which maps the true state space into the observed space ( P, Hk )
+  mat I(4, 4, fill::eye);             // identity matrix
+  mat H(2, 2, fill::zeros);           // the covariance of the observation noise ( error, Rk )
+  cube T(4, 4, nbData, fill::zeros);  // state transition model which is applied to the previous state xk−1 ( Green, Fk )
+  cube Q(4, 4, nbData, fill::zeros);  // covariance of the process noise ( Sigma, Q )
+  mat K(4, 2, fill::zeros);           // Kalman Gain ( Gain, Kk )
+  mat u(nbData, 2, fill::zeros);      // measurement residual ( zRes, yk )
+  cube iF(2, 2, nbData, fill::zeros); // inverse of residual covariance, residual "precision" ( isRes )
 
-  cube L( 4, 4, nbData, fill::zeros );
-
-  // vector to keep track of the indices for first positions that are IOU, and size
-  uvec iou(nbData);
-  int k = 0;
+  cube L(4, 4, nbData, fill::zeros);
 
   // forward filter iterations
-  for( int i = 0; i < nbData; i++ ) {
+  for (int i = 0; i < nbData; i++)
+  {
 
     // if first location or new individual
-    if( i == 0 || ID( i ) != ID( i - 1 ) ) {
-      nbID++;
-      // if starting in IOU, keep track of index
-      if( std::isinf( tau_pos( S(i) - 1 ) ) ) {
-        iou(k) = i;
-        k++;
-      }
+    if (i == 0 || ID(i) != ID(i - 1))
+    {
       // reset dt to inf (when new individual)
       dt(i) = datum::inf;
 
       // initialise state mean
-      afor.row(i) = zeros( 1, 4 );
+      afor.row(i) = zeros(1, 4);
       // and initial state covariance matrix
-      Pfor.slice(i) = makeQ( tau_pos( S(i) - 1 ), tau_vel( S(i) - 1 ), sigma.slice( S(i) - 1 ), dt(i) );
+      Pfor.slice(i) = makeQ(tau_pos(S(i) - 1), tau_vel(S(i) - 1), sigma.slice(S(i) - 1), dt(i));
     }
 
     // update our estimate ( if, missing obs skip to prediction )
-    if(!R_IsNA( X(i, 0)) && !R_IsNaN(X(i, 0))) {
-      H( 0, 0 ) = Hmat( i, 0 );
-      H( 1, 1 ) = Hmat( i, 1 );
-      H( 0, 1 ) = Hmat( i, 2 );
-      H( 1, 0 ) = Hmat( i, 3 );
+    if (!R_IsNA(X(i, 0)) && !R_IsNaN(X(i, 0)))
+    {
+      H(0, 0) = Hmat(i, 0);
+      H(1, 1) = Hmat(i, 1);
+      H(0, 1) = Hmat(i, 2);
+      H(1, 0) = Hmat(i, 3);
       // measurement residual (zRes, u)
-      u.row(i) = X.row(i) - ( Z * afor.row(i).t() ).t();
+      u.row(i) = X.row(i) - (Z * afor.row(i).t()).t();
 
       // residual covariance (sRes, F)
       mat PforZt = Pfor.slice(i) * Z.t();
-      PforZt = PforZt.replace( datum::nan, 0 );
+      PforZt.replace(datum::nan, 0);
       mat ZPforZt = Z * PforZt;
-      ZPforZt = ZPforZt.replace( datum::nan, 0 );
-      mat F = ZPforZt + H;                                //residual covariance ( sRes, Sk )
+      ZPforZt.replace(datum::nan, 0);
+      mat F = ZPforZt + H; // residual covariance ( sRes, Sk )
       iF.slice(i) = F.i();
 
       // Kalman gain
       K = PforZt * iF.slice(i);
-      //if gain is inf or nan, replace with Z
-      uvec idx = find_nonfinite( K );
+      // if gain is inf or nan, replace with Z
+      uvec idx = find_nonfinite(K);
       mat Zt = Z.t();
       K(idx) = Zt(idx);
 
       // update concurrent state estimate (zCon, aest)
-      aest.row(i) = afor.row(i) + ( K * u.row(i).t() ).t();
+      aest.row(i) = afor.row(i) + (K * u.row(i).t()).t();
       // update concurrent covariance estimate (sCon, Pest)
-      mat J = I - ( K * Z );
+      mat J = I - (K * Z);
       mat JPest = J * Pfor.slice(i);
-      JPest = JPest.replace( datum::nan, 0 );
+      JPest.replace(datum::nan, 0);
       mat KHKt = K * H * K.t();
-      KHKt = KHKt.replace( datum::nan, 0 );
+      KHKt.replace(datum::nan, 0);
       Pest.slice(i) = JPest * J.t() + KHKt;
     }
 
-
     // proceed with forecast
-    if( i < nbData - 1 && ID( i ) == ID( i + 1 ) ) {
-      T.slice(i) = makeT( tau_pos( S( i + 1 ) - 1 ), tau_vel( S( i + 1 ) - 1), dt( i + 1 ) );
-      Q.slice(i) = makeQ( tau_pos( S( i + 1 ) - 1 ), tau_vel( S( i + 1 ) - 1), sigma.slice( S( i + 1 ) - 1 ), dt( i + 1 ) );
+    if (i < nbData - 1 && ID(i) == ID(i + 1))
+    {
+      T.slice(i) = makeT(tau_pos(S(i + 1) - 1), tau_vel(S(i + 1) - 1), dt(i + 1));
+      Q.slice(i) = makeQ(tau_pos(S(i + 1) - 1), tau_vel(S(i + 1) - 1), sigma.slice(S(i + 1) - 1), dt(i + 1));
 
-      //predict state estimate (zFor, aest)
+      // predict state estimate (zFor, aest)
       afor.row(i + 1) = (T.slice(i) * aest.row(i).t()).t();
 
-      //predict covariance estimate (sFor, Pest)
+      // predict covariance estimate (sFor, Pest)
       mat tcon = Pest.slice(i);
-      bool any = tcon.has_inf();
-      if( any ) {
-        tcon = tcon.replace( datum::inf, 0 );
+      bool has_inf = tcon.has_inf();
+      if (has_inf)
+      {
+        tcon = tcon.replace(datum::inf, 0);
       }
 
       Pfor.slice(i + 1) = T.slice(i) * tcon * T.slice(i).t() + Q.slice(i);
 
-      if( any ) {
+      if (has_inf)
+      {
         vec Pdiag = Pfor.slice(i + 1).diag();
         bool anyP = Pdiag.has_inf();
         uvec idx(4);
         int l = 0;
-        if( anyP ) {
-          for( int j = 0; j < Pdiag.size(); j++ ){
-            if( std::isinf( Pdiag(j) ) ){
+        if (anyP)
+        {
+          for (uword j = 0; j < Pdiag.size(); j++)
+          {
+            if (std::isinf(Pdiag(j)))
+            {
               idx(l) = j;
               l++;
             }
           }
-          Pfor.slice(i + 1).rows( idx.head(l) ).zeros();
-          Pfor.slice(i + 1).cols( idx.head(l) ).zeros();
+          Pfor.slice(i + 1).rows(idx.head(l)).zeros();
+          Pfor.slice(i + 1).cols(idx.head(l)).zeros();
           vec Pdiag2 = Pfor.slice(i + 1).diag();
-          Pdiag2( idx.head(l) ).fill( datum::inf );
+          Pdiag2(idx.head(l)).fill(datum::inf);
           Pfor.slice(i + 1).diag() = Pdiag2;
         }
       }
     }
   } // end filter iterations
 
-  // shed all missing (skipped) obs
-  //uvec na_xy = find_nonfinite( X.col(0) );
-  //aest.shed_rows( na_xy );
-  //afor.shed_rows( na_xy );
-  //Pest.shed_slices( na_xy );
-  //T.shed_slices( na_xy );
-  //Q.shed_slices( na_xy );
-
-  //backward smoother
-  for (int j = aest.n_rows - 2; j >= 0; j--) {
-    if(ID(j) == ID(j + 1)) {
+  // backward smoother
+  for (uword j = aest.n_rows - 2; j >= 0; j--)
+  {
+    if (ID(j) == ID(j + 1))
+    {
       mat TL(4, 4);
-      if(!R_IsNA(X(j, 0)) && !R_IsNaN(X(j, 0))) {
+      if (!R_IsNA(X(j, 0)) && !R_IsNaN(X(j, 0)))
+      {
         mat TL = Pest.slice(j) * T.slice(j).t();
-      } else {
+      }
+      else
+      {
         mat TL = Pfor.slice(j) * T.slice(j).t();
       }
-      TL = TL.replace(datum::inf, 0);
-      //mat INV = solve(Pfor.slice(j + 1), eye(4, 4));
+      TL.replace(datum::inf, 0);
+      // mat INV = solve(Pfor.slice(j + 1), eye(4, 4));
       mat INV = PDsolve(Pfor.slice(j + 1));
       TL = TL * INV;
 
       vec TLdiag = TL.diag();
-      bool anyTL = TLdiag.has_nan();
+      bool has_nan = TLdiag.has_nan();
       uvec idx = find_nonfinite(TLdiag);
-      if(anyTL) {
+      if (has_nan)
+      {
         TL.rows(idx).zeros();
         TL.cols(idx).zeros();
         vec TLdiag2 = TL.diag();
@@ -217,27 +217,30 @@ List smooth_rcpp(const arma::mat& data, int nbStates, const arma::vec param, con
 
       L.slice(j) = TL;
       mat J = I - (L.slice(j) * T.slice(j));
-      if(!R_IsNA(X(j, 0)) && !R_IsNaN(X(j, 0))) {
+      if (!R_IsNA(X(j, 0)) && !R_IsNaN(X(j, 0)))
+      {
         mat JPest = J * Pest.slice(j);
-        JPest = JPest.replace(datum::nan, 0);
+        JPest.replace(datum::nan, 0);
         Pest.slice(j) = JPest * J.t() + (L.slice(j) * (Pest.slice(j + 1) + Q.slice(j)) * L.slice(j).t());
         aest.row(j) = aest.row(j) + (L.slice(j) * (aest.row(j + 1) - afor.row(j + 1)).t()).t();
-      } else {
+      }
+      else
+      {
         // prediction (no est)
         mat JPest = J * Pfor.slice(j);
-        JPest = JPest.replace(datum::nan, 0);
+        JPest.replace(datum::nan, 0);
         Pest.slice(j) = JPest * J.t() + (L.slice(j) * (Pest.slice(j + 1) + Q.slice(j)) * L.slice(j).t());
         aest.row(j) = afor.row(j) + (L.slice(j) * (aest.row(j + 1) - afor.row(j + 1)).t()).t();
       }
-    } else {
+    }
+    else
+    {
       Pest.slice(j) = Pest.slice(j);
       aest.row(j) = aest.row(j);
     }
   }
 
   return List::create(
-    Rcpp::Named("pred") = DataFrame::create( Named("x") = aest.col(0), Named("vx") = aest.col(1), Named("y") = aest.col(2), Named("vy") = aest.col(3) ),
-    Rcpp::Named("predVar") = Pest
-    );
-
+      Rcpp::Named("pred") = DataFrame::create(Named("x") = aest.col(0), Named("vx") = aest.col(1), Named("y") = aest.col(2), Named("vy") = aest.col(3)),
+      Rcpp::Named("predVar") = Pest);
 }
