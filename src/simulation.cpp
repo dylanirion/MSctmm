b4e52807dfb48ea2974285f2b8c1770586cd11a3
dylanirion <dylan.irion@comcast.net>
2024-06-07 11:50:01 +0200

    increase iteration limit

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -23,7 +23,7 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 arma::mat sample_path_mr(const int a, const int b, const double t0, const double t1, const Rcpp::NumericMatrix &Q, const double k)
 {
-  const int limit = 1000;
+  const int limit = 10000;
   const int sublimit = std::ceil((t1 - t0) * k) * 100;
 
   // Get the number of states and initialize vector of states
